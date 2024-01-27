c5765b4642f5ec30c52e1b84c2888d70a09d86d1
DavidUrting <david.urting@gmail.com>
2024-01-27 16:21:33 +0100

    Terug in sync met hoofdstuk 1

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 import { Car } from "./car.js";
 import { AutomaticCar } from "./automatic-car.js";
 
-let honda = new Car("Honda","Groen","Diesel");
+let honda = new Car("Honda", "Groen", 145); // Het ‘new’ keyword maakt een object aan van class
 honda.start();
 honda.gearUp();
-honda.move(10);
-// ...
+honda.acceleratorPedalPosition = 0.5;
+honda.move();
 honda.gearUp();
-honda.move(20);
-// ...
+honda.move();
 
-let audi = new Car("Audi","Grijs","Benzine");
-audi.start();
-// ...
+let audi = new Car("Audi", "Grijs", 220);
+audi.move();
 
-let vw = new AutomaticCar("Volkswagen","Wit","Benzine");
+let vw = new AutomaticCar("Volkswagen", "Wit", 180);
 vw.start();
-vw.gearMode = "Drive";
-vw.move(10);
-// ...
-vw.move(50);
-// ...
-vw.move(100); // “pedal to the metal”
-// ...
+vw.gearMode = "D";
+vw.acceleratorPedalPosition = 0.2;
+vw.move();
+vw.acceleratorPedalPosition = 0.5;
+vw.move();
+vw.acceleratorPedalPosition = 1; // “pedal to the metal” 
+vw.move();
